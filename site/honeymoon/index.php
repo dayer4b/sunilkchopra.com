bd9a3d47eb2cc6f39c5cf2f9a7f1d908c562b1aa
sunil <sunil@sunilkchopra.com>
2012-10-26 22:26:06 -0500

    added honeymoon landing page

---
+++ site/honeymoon/index.php
@@ -0,0 +1,107 @@
+<?php
+
+$media_path = "media/";
+$files;
+
+if ($handle = opendir($media_path)) {
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry != "." && $entry != "..") {
+            $files[] = trim($entry);
+        }
+    }
+    closedir($handle);
+}
+sort($files);
+
+
+/*
+
+		<ul>
+			<?php
+			foreach($files as $file){
+			?>
+			<li><a href='<?php echo $media_path.$file; ?>'><?php echo $file; ?></a></li>	
+			<?php
+			}
+			?>
+		</ul>
+
+
+*/
+?>
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title>Honey Moon by Sunil K Chopra</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<meta name="viewport" content="width=device-width">
+	
+    <link rel="shortcut icon" href="img/icon.jpg">
+    <link rel="apple-touch-icon" href="img/icon.jpg">
+	<link rel="stylesheet" href="css/style.css">
+
+	<script src="js/libs/modernizr-2.5.3.min.js"></script>
+</head>
+<body>
+<div class='container'>
+	<header>
+		<h1>Honey Moon</h1>
+		<h2>by Sunil K Chopra</h2>
+	</header>
+	<div role="main">
+		
+		<div class="album">
+			<a href='img/full-cover.jpg' id='album' title='click to see the full cover'>the album</a>
+		</div>
+		<div class="player">
+			<iframe id="playerLarge" width="300" height="450" style="position: relative; display: block; width: 300px; height: 450px;" src="http://bandcamp.com/EmbeddedPlayer/v=2/album=2800233047/size=grande3/bgcol=FFFFFF/linkcol=4285BB/" allowtransparency="true" frameborder="0"><a href="http://sunilkchopra.bandcamp.com/album/honey-moon">Honey Moon by Sunil K Chopra</a></iframe>
+			<iframe id="playerSmall" width="150" height="570" style="position: relative; display: block; width: 150px; height: 570px;" src="http://bandcamp.com/EmbeddedPlayer/v=2/album=2800233047/size=tall2/bgcol=FFFFFF/linkcol=4285BB/" allowtransparency="true" frameborder="0"><a href="http://sunilkchopra.bandcamp.com/album/honey-moon">Honey Moon by Sunil K Chopra</a></iframe>
+			<a href='http://sunilkchopra.bandcamp.com/album/honey-moon'>Honey Moon on Bandcamp</a>
+		</div>
+		<div class="information">
+			<h3>About the album</h3>
+			<p>This album was recorded entirely in February 2012 as part of my first successful participation in the <a href='http://www.rpmchallenge.com'>RPM Challenge</a>.  The rules stipulate that a successful entry must be completely recorded in the month of February and submitted for approval before noon on March first.  Although they recommend that the songs be written in February as well, it is not a requirement.</p>
+			<p>On that note, these songs were not written in February.  The majority of these songs were written in August of 2011 and refined in the time since.  For our honeymoon, Liz and I took a train ride down the Mississippi River to New Orleans, then back through Memphis on our return to Chicago.  Accordingly, this album is called <strong>"Honey Moon"</strong>.</p>
+			<p>I've decided to license these songs here under the <a href='http://creativecommons.org/licenses/by-sa/3.0/'>CC BY SA (3.0) license</a>.  This means that the songs are free to anybody to copy and distribute as long as they are credited to me.  Additionally, derivative works from this music are freely allowed with credit given as long as they are produced under a similarly permissive license.</p>
+		</div>
+	</div>
+	<footer>
+		<section>
+			<h3>Credits</h3>
+			<p>All songs written by <a href='http://www.sunilkchopra.com'>Sunil K Chopra</a>.</p>
+			<p>Recorded and produced by <a href='http://www.sunilkchopra.com'>Sunil K Chopra</a>.</p>
+			<p><a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.</p>
+			
+		</section>
+	</footer>
+</div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+
+<script src="js/plugins.js" defer></script>
+<script src="js/script.js" defer></script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-29790902-1']);
+  _gaq.push(['_setDomainName', 'sunilkchopra.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</body>
+</html>
